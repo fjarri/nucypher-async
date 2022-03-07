35592f1e6c138e5be153d6aef735271a18068c4a
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-03-07 00:04:47 -0800

    drivers/rest_app: catch all errors from request handlers so that they don't crash the server

--- nucypher_async/drivers/rest_app.py
+++ nucypher_async/drivers/rest_app.py
@@ -12,19 +12,26 @@ is located in ``UrsulaServer``.
 In a sense, this is a "server" counterpart of ``rest_client``.
 """
 
+import http
+import sys
+
 from quart_trio import QuartTrio
 from quart import make_response, request
 
 from .rest_client import HTTPError
 
 
-async def wrap_in_response(callable, *args, **kwds):
+async def wrap_in_response(logger, callable, *args, **kwds):
     try:
         result_bytes = await callable(*args, **kwds)
     # TODO: we can have a small subset of errors here that are a part of the protocol,
     # and correspond to HTTP status codes
     except HTTPError as e:
         return await make_response((str(e), e.status_code))
+    except Exception as e:
+        # A catch-all for any unexpected errors
+        logger.error("Uncaught exception:", exc_info=sys.exc_info())
+        return await make_response((str(e), http.HTTPStatus.INTERNAL_SERVER_ERROR))
     return await make_response(result_bytes)
 
 
@@ -38,6 +45,11 @@ def make_app(ursula_server):
 
     app = QuartTrio('ursula_async')
 
+    # TODO: this is a little backwards; the app encompasses UrsulaServer,
+    # but this new logger is a child of UrsulaServer's logger.
+    # Is there a more logical way to create a logger?
+    logger = ursula_server._logger.get_child('App')
+
     @app.before_serving
     async def on_startup():
         ursula_server.start(app.nursery)
@@ -48,24 +60,24 @@ def make_app(ursula_server):
 
     @app.route("/ping")
     async def ping():
-        return await wrap_in_response(ursula_server.endpoint_ping, request.remote_addr)
+        return await wrap_in_response(logger, ursula_server.endpoint_ping, request.remote_addr)
 
     @app.route("/node_metadata")
     async def node_metadata_get():
-        return await wrap_in_response(ursula_server.endpoint_node_metadata_get)
+        return await wrap_in_response(logger, ursula_server.endpoint_node_metadata_get)
 
     @app.route("/node_metadata", methods=['POST'])
     async def node_metadata_post():
         metadata_request_bytes = await request.data
-        return await wrap_in_response(ursula_server.endpoint_node_metadata_post, metadata_request_bytes)
+        return await wrap_in_response(logger, ursula_server.endpoint_node_metadata_post, metadata_request_bytes)
 
     @app.route("/public_information")
     async def public_information():
-        return await wrap_in_response(ursula_server.endpoint_public_information)
+        return await wrap_in_response(logger, ursula_server.endpoint_public_information)
 
     @app.route("/reencrypt", methods=["POST"])
     async def reencrypt():
         reencryption_request_bytes = await request.data
-        return await wrap_in_response(ursula_server.endpoint_reencrypt, reencryption_request_bytes)
+        return await wrap_in_response(logger, ursula_server.endpoint_reencrypt, reencryption_request_bytes)
 
     return app
