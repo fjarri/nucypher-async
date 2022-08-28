8d5bd2e0f4e448e3f1914eb5d95da32dcadd60d9
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-08-27 17:03:55 -0700

    Typing for version

--- nucypher_async/version.py
+++ nucypher_async/version.py
@@ -1,26 +1,26 @@
 import os
 from pathlib import Path
 import subprocess
-from typing import Optional, List
+from typing import Optional, List, Sequence
 
-from attrs import define
+from attrs import frozen
 import pkg_resources
 
 
-def _run_in_project_dir(cmd):
+def _run_in_project_dir(cmd: Sequence[str]) -> bytes:
     cwd = Path(__file__).parent
-    out = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd).communicate()[0]
-    return out
+    stdout_data, _stderr_data = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd).communicate()
+    return stdout_data
 
 
-@define
+@frozen
 class FileDiff:
     path: str
     added: int
     removed: int
 
 
-@define
+@frozen
 class CodeInfo:
     version: str
     release: bool
@@ -28,13 +28,13 @@ class CodeInfo:
     diff: List[FileDiff]
 
     @classmethod
-    def collect(cls):
+    def collect(cls) -> "CodeInfo":
         version = pkg_resources.get_distribution("nucypher-async").version
         revision = git_revision()
 
         if revision:
             tag = git_tag()
-            release = tag and "v" + tag == version
+            release = tag is not None and "v" + version == tag
             diff = git_diff()
         else:
             release = True
@@ -43,7 +43,7 @@ class CodeInfo:
         return cls(version=version, release=release, git_revision=revision, diff=diff)
 
 
-def git_revision():
+def git_revision() -> Optional[str]:
     try:
         out = _run_in_project_dir(["git", "rev-parse", "HEAD"])
     except OSError:
@@ -52,7 +52,7 @@ def git_revision():
     return revision or None
 
 
-def git_tag():
+def git_tag() -> Optional[str]:
     try:
         out = _run_in_project_dir(["git", "tag", "--points-at", "HEAD"])
     except OSError:
@@ -61,7 +61,7 @@ def git_tag():
     return tag or None
 
 
-def git_diff():
+def git_diff() -> List[FileDiff]:
     try:
         out = _run_in_project_dir(["git", "diff", "--numstat"])
         diff_str = out.strip().decode()
@@ -76,4 +76,4 @@ def git_diff():
             diff.append(FileDiff(path=path, added=int(added), removed=int(removed)))
         return diff
     else:
-        return None
+        return []
